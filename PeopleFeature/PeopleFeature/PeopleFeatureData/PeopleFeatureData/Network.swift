ebdb0df41f28dbac16e7f4dbd5d183f18861165e
Daniel Bastidas <danielbastidasramirez@gmail.com>
2019-10-16 20:48:10 +0100

    Add: Data project for every feature

---
+++ PeopleFeature/PeopleFeature/PeopleFeatureData/PeopleFeatureData/Network.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  PeopleFeatureData
+//
+//  Created by Daniel Bastidas Ramirez on 16/10/2019.
+//  Copyright © 2019 Daniel Bastidas. All rights reserved.
+//
+
+import Foundation
+
+public class Network {
+    public init() {
+        
+    }
+}
